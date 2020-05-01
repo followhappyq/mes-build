29406655af2d02e1f1b4390a5d6b53bba6dca509
Ivan <followhappyq@gmail.com>
2020-05-01 10:46:44 +0200

    build

---
+++ precache-manifest.958cf83cc5782a1afa590169d78df0cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcda83a0a93859c8f08dd66aba672962",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adddcfeda3e69848dde4",
+    "url": "/static/css/2.4a42413a.chunk.css"
+  },
+  {
+    "revision": "20e33af1d6e49de98c16",
+    "url": "/static/css/main.5974b93c.chunk.css"
+  },
+  {
+    "revision": "adddcfeda3e69848dde4",
+    "url": "/static/js/2.2e6101a0.chunk.js"
+  },
+  {
+    "revision": "1a2f2540a78b2022a70889f2de7372f4",
+    "url": "/static/js/2.2e6101a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e33af1d6e49de98c16",
+    "url": "/static/js/main.b42ffd09.chunk.js"
+  },
+  {
+    "revision": "3c153ba9f7fe429eec86",
+    "url": "/static/js/runtime-main.bb504404.js"
+  },
+  {
+    "revision": "1456f01338efe100733984d48807dd08",
+    "url": "/static/media/noreaded.1456f013.svg"
+  },
+  {
+    "revision": "2da46147a19449255266514c196ca9cc",
+    "url": "/static/media/readed.2da46147.svg"
+  }
+]);
